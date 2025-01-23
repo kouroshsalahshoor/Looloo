10f1172e161de2416ef13597d10db551689e9f00
kourosh <kourosh23@hotmail.com>
2025-01-23 12:48:48 +0100

    Add project files.

---
+++ Looloo.BlazorServer/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace Looloo.BlazorServer.Models
+{
+    public class Department
+    {
+        public long Departmentid { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public IEnumerable<Person>? People { get; set; }
+    }
+}

87ec80d6d53f77a48cf436af4d63f4bc19c8dbc4
kourosh <kourosh23@hotmail.com>
2025-01-29 10:52:07 +0100

    coop company icon

--- Looloo.BlazorServer/Services/Food/CoopService.cs
+++ Looloo.BlazorServer/Services/Food/CoopService.cs
@@ -20,19 +20,18 @@ namespace Looloo.BlazorServer.Services.Food
             //_driver = _driver ?? new ChromeDriver();
 
             _driver.Navigate().GoToUrl($"https://www.coop.se/handla/sok/?q={searchTerm.Trim()}");
-            //_driver.Url = $"https://www.coop.se/handla/sok/?q={searchTerm.Trim()}";
             closeCookieWindow();
 
-            var elements = _driver.FindElements(By.CssSelector("div.ProductTeaser-info"));
+            var elements = _driver.FindElements(By.XPath("//*[@id=\"pageContent\"]/div/div[5]/div/div/div"));
             if (elements is not null)
             {
-
                 foreach (var element in elements)
                 {
                     var title = SeleniumExtensions.findElementByCss(element, "div.ProductTeaser-headingContainer > p > a");
                     var price = SeleniumExtensions.findElementByCss(element, "div.ProductTeaser-detailsContainer > div.SnZ2rtnf.sJx8F6kR.ProductTeaser-pricesContainer > span > span:nth-child(1)");
                     var size = SeleniumExtensions.findElementByCss(element, "div.ProductTeaser-headingContainer > div > div");
                     var sizePrice = SeleniumExtensions.findElementByCss(element, "div.ProductTeaser-detailsContainer > div.ProductTeaser-details > div:nth-child(2)");
+                    var imageUrl = SeleniumExtensions.findElementByCss(element, "div.ProductTeaser-media > a > div > img");
 
                     //price = price.Replace("kr", "").Replace(",", ".").Trim();
                     //sizePrice = sizePrice.Replace("(", "").Replace(")", "").Replace(",", ".").Trim();
@@ -46,39 +45,10 @@ namespace Looloo.BlazorServer.Services.Food
                         SizePrice = sizePrice == null ? string.Empty : sizePrice.Text,
                         //SizePrice = decimal.Parse(sizePrice),
                         Company = "Coop",
+                        ImageUrl = imageUrl == null ? string.Empty : imageUrl.GetAttribute("src")
                     });
                 }
 
-            }
-
-            _driver.Quit();
-
-            return result;
-        }
-
-        public async Task<List<Category>> GetCategories()
-        {
-            _driver = _driver ?? new ChromeDriver();
-
-            _driver.Url = "https://handlaprivatkund.ica.se/stores/1003988/categories?source=navigation";
-            closeCookieWindow();
-
-            var result = new List<Category>();
-
-            //https://toolsqa.com/selenium-webdriver/c-sharp/findelement-and-findelements-commands-in-c/
-            var elements = _driver.FindElements(By.CssSelector("#product-page > div > div._grid-item-12_tilop_45._grid-item-lg-2_tilop_249 > div.sc-1hfavqh-0.bhvoGf > ul > li > a"));
-            //var categories = _driver.FindElements(By.CssSelector("#nav-menu > li > a > div > span"));
-            if (elements is not null)
-            {
-
-                foreach (var element in elements)
-                {
-                    result.Add(new Category
-                    {
-                        Name = element.GetAttribute("text")
-                        //Title = element.Text
-                    });
-                }
             }
 
             _driver.Quit();
@@ -97,26 +67,10 @@ namespace Looloo.BlazorServer.Services.Food
                 {
                     cookieButton.Click();
                 }
-
-                //Thread.Sleep(1000);
-
-                //var loginButton = _driver?.FindElement(By.XPath(".//button[contains(@class, 'gUGSFhfR CkqGWkRo ucdesrxw qfkHWAKt')]"));
-                //if (loginButton is not null)
-                //{
-                //    loginButton.Click();
-                //}
             }
             catch (Exception e)
             {
             }
         }
-
-        //public void Dispose()
-        //{
-        //    if (_driver is not null)
-        //    {
-        //        _driver?.Quit();
-        //    }
-        //}
     }
 }
